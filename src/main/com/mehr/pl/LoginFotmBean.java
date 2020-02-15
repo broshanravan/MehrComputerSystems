cda81bf9895bb6fcce4271f16b5cdd492285f472
Behrooz Roshanravan <broshanravan@hotmail.com>
2020-02-15 13:24:08 +0000

    syncronizing with remote

--- src/main/com/mehr/pl/LoginFotmBean.java
+++ src/main/com/mehr/pl/LoginFotmBean.java
@@ -3,5 +3,26 @@ package main.com.mehr.pl;
 /**
  * Created by Behrooz on 15/07/2017.
  */
+
+@Bean
 public class LoginFotmBean {
+    private String userName;
+    private String password;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
